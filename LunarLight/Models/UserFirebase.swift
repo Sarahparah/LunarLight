914a65c8b58e2a9b70f3538242dd8e2837057543
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-04-07 08:02:03 +0200

    Modified UserFirebase-class: -UUID no longer generated in constructor (needs to be generated outside). -Added field for avatar (String).

--- LunarLight/Models/UserFirebase.swift
+++ LunarLight/Models/UserFirebase.swift
@@ -10,21 +10,23 @@ import FirebaseFirestoreSwift
 
 struct UserFirebase: Codable, Identifiable{
     
-    var id: String = UUID().uuidString
+    var id: String
     var username: String
     var email: String
     var password: String
     var year: UInt64
     var month: UInt64
+    var avatar: String
     
-    init(_username: String, _email: String, _password: String, _year: UInt64, _month: UInt64 ){
+    init(_id: String = "", _username: String, _email: String, _password: String, _year: UInt64, _month: UInt64, _avatar: String ){
         
-        
+        id = _id
         username = _username
         email = _email
         password = _password
         year = _year
         month = _month
+        avatar = _avatar
     }
     
 }
