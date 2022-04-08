0fdd724e32b1948bdae92d1a9d73d0bcb0813e24
Hampus <hampus.brandtman@student.kyh.se>
2022-04-08 15:55:15 +0200

    changed design in chatSubView and WorldChatView

--- LunarLight/SubViews/MessageView.swift
+++ LunarLight/SubViews/MessageView.swift
@@ -18,7 +18,7 @@ struct MessageView: View {
     @State var backround: String
     
     init(_user: String, _message: String, _avatar: String, _month: UInt64, _day: UInt64){
-    
+        
         let localData = LocalData()
         user = _user
         message = _message
@@ -34,29 +34,38 @@ struct MessageView: View {
     var body: some View {
         
         VStack {
-            Divider()
-                .padding(2)
-                .opacity(0)
-            
-            HStack {
-                
-                Image(avatar)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 40, height: 40)
-                    .padding(5)
-                
-                Text("\(user): \(message)")
-                    .padding()
+            //            Divider()
+            //                .padding(2)
+            //                .opacity(0)
+            HStack{
+                HStack(alignment:.top) {
                     
-            }
-            .background(Color(backround))
+                    Image(avatar)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                        .background(.white)
+                        .cornerRadius(30)
+                        .padding(8)
+                    
+                    Text("\(user): \(message)")
+                        .padding()
+                    
+                    Spacer()
+                    
+                    
+                }
+                .frame(width: UIScreen.main.bounds.size.width * 0.7)
+                .background(Color(backround))
                 .cornerRadius(30)
+                .padding(5)
+                
+                Spacer()
+            }
             
-            
-            Divider()
-                .padding(2)
-                .opacity(0)
+            //            Divider()
+            //                .padding(2)
+            //                .opacity(0)
         }
     }
 }
