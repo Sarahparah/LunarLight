555e10a6b3e77aa97841e7e80c00c3420d5572b3
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-04-19 16:46:45 +0200

    change the encryption logarithm

--- LunarLight/Utils/Encryption.swift
+++ LunarLight/Utils/Encryption.swift
@@ -15,37 +15,25 @@ class Encryption {
         var token = ""
         
         var i = 0
-        for char in input {
+        for char in Array(input) {
             
             //get ascii value for current char
-            var asciiValue: UInt8 = char.asciiValue ?? UInt8(32)
+            let asciiValue: UInt = UInt(char.asciiValue ?? UInt8(32))
             
-            //if asciivalue is lower than UInt8.max, increase with 1
-            if asciiValue < UInt8.max {
-                asciiValue += UInt8(i)
+            if i % 3 == 0 {
+                print("DanneToken 1")
+                token += String(asciiValue * 2)
             }
-            
-            //if asciiValue is greater than 255, remove 255 from value
-            if asciiValue > 255 {
-                asciiValue -= 255
+            else if i % 2 == 0 {
+                print("DanneToken 2")
+                token += String(asciiValue * 3)
             }
-            //if asciiValue is lower than 32, add 32 to the value
-            if asciiValue < 32 {
-                asciiValue += 32
-            }
-            
-            //on even indexes: append the asciiValue
-            if i % 2 == 0 {
-                token += "\(asciiValue)"
-            }
-            //on un-even indexes: apply the new char
             else {
-                token += String(UnicodeScalar(asciiValue))
+                print("DanneToken 3")
+                token += "_\(asciiValue)"
             }
             
-            print("DanneToken: \(i) = \(token)")
-            
-            i += i
+            i += 1
         }
         
         //finally return the encrypted string
